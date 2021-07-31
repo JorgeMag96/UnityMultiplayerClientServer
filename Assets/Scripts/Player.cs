90ae9256e27f8c78357dd97e025454748e08b977
Jorge Villarreal <jorge.vi96@gmail.com>
2021-07-31 17:50:13 -0500

    Hello world, multiplayer edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour {
+
+    void HandleMovement()
+    {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertial = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertial * 0.1f, 0);
+            transform.position += movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
