dac7107c51ac10644db2ba17eb7695f71d4e70d1
Jorge Villarreal <jorge.vi96@gmail.com>
2021-08-02 07:19:16 -0500

    Added terrain and camera modifications to follow player prefab from an RTS angle

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; //what to follow
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate() {
+        if (target != null)
+        {
+            transform.position = target.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour {
+
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.GetComponent<CameraFollow>().target=transform; //Fix camera on "me" 
+    }
+
+    void HandleMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal * 0.1f, 0,  moveVertical * 0.1f);
+        transform.position += movement;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        HandleMovement();
+    }
+}
